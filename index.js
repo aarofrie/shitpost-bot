861dd64c5c80672e726775d60767f51690a40e4c
Chris Bacon <cbacon91@gmail.com>
2018-03-14 15:33:19 -0600

    refactor to make future maintenance easier; added 🤢 reply

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('./config');
+const responses = require('./responses');
 
 // wait five minutes and try again .. The most common crash is discord losing connection,
 // and trying again immediately would fail as well.
@@ -31,8 +32,6 @@ function init() {
   const bot = new Discord.Client();
   const token = config.discord.authToken;
 
-
-
   bot.login(token);
 
   bot.on('message', onMessage);
@@ -58,129 +57,6 @@ function init() {
       .filter(r => r.trigger(msg, rnd))
       .forEach(r => r.effect(msg));
   }
-}
-
-const baconId = '108352053692125184';
-const zackId =  '108568431053246464';
-const zackMoji = ':zack:401543766084943892';
-const rizoId = '100758264047747072'
-const parrotMoji = 'a:ultrafastparrot:397874139848769557';
-const jerranId = '189006310501646336';
-const aaronId = '65055432095301632';
-
-const responses = [
-  {
-    name: 'react to zack with zackMoji',
-    trigger: (msg, rnd) => msg.author.id === zackId && rnd <= 0.2,
-    effect: (msg) => msg.react(zackMoji)
-  },
-  {
-    name: '@zack',
-    trigger: (msg, rnd) => msg.content.includes(`@<:zack:401543766084943892>`),
-    effect: (msg) => msg.channel.send(`<@${zackId}>`)
-  },
-  {
-    name: '@bacon',
-    trigger: (msg, rnd) => msg.content.includes(`@🥓`),
-    effect: (msg) => msg.channel.send(`<@${baconId}>`)
-  },
-  {
-    name: 'rizo parrot wave',
-    trigger: (msg, rnd) => msg.author.id === rizoId && rnd < 0.05,
-    effect: reactWave
-  },
-  {
-    name: 'rizo party',
-    trigger: (msg, rnd) => msg.author.id === rizoId && rnd >= 0.05 && rnd < 0.15,
-    effect: (msg) => msg.react('a:partyparrot:397874122232954901')
-  },
-  {
-    name: 'rizo partyfast',
-    trigger: (msg, rnd) => msg.author.id === rizoId && rnd >= 0.15 && rnd <= 0.2,
-    effect: (msg) => msg.react('a:ultrafastparrot:397874139848769557')
-  },
-  {
-    name: 'rizo pls',
-    trigger: (msg, rnd) => msg.content.includes('<@100758264047747072>'),
-    effect: rizoPls
-  },
-  {
-    name: 'lets go party',
-    trigger: (msg, rnd) => (msg.content.strip().includes('letsgo') || msg.content.strip().includes('ketsgi')) && rnd < 0.5,
-    effect: (msg) => msg.react('a:partyparrot:397874122232954901')
-  },
-  {
-    name: 'lets go letters',
-    trigger: (msg, rnd) => (msg.content.strip().includes('letsgo') || msg.content.strip().includes('ketsgi')) && rnd >= 0.5,
-    effect: letsGoLetters
-  },
-  {
-    name: 'namelessGing',
-    trigger: (msg, rnd) => msg.author.id === jerranId && rnd <= 0.2,
-    effect: (msg) => msg.react('a:wendyparrot:399242434300870658')
-  },
-  {
-    name: 'aaron gif reply',
-    trigger: (msg, rnd) => msg.author.id === aaronId && rnd <= 0.01,
-    effect: aaronGif 
-  },
-  {
-    name: 'aaron bepsi reply',
-    trigger: (msg, rnd) => msg.author.id === aaronId && rnd <= 0.15,
-    effect: (msg) => msg.react(':bepsi:410166385918869504')
-  },
-  {
-    name: 'make [thing] great again',
-    trigger: (msg, rnd) => msg.content.toLowerCase().match(/.*ma[kd]e.*great again.*/),
-    effect: (msg) => msg.react(':maga:423325774674788352')
-  },
-  {
-    name: 'maga -> trumpgasm',
-    trigger: (msg, rnd) => msg.content.includes(':maga:'),
-    effect: (msg) => msg.react(':trumpgasm:416058928502276106')
-  }
-];
-
-function aaronGif(msg) {
-  // let's keep this bad boy around for a bit 
-  // msg.channel.send('https://giphy.com/gifs/man-pepsi-9kDr8Zm8dOuyc');
-  msg.channel.send('https://giphy.com/gifs/soda-funny-hot-G1zGMZtfmKjEQ');
-  msg.channel.send(`<@${aaronId}> irl`);
-}
-
-
-function reactWave(msg) {
-  msg.react('a:parrotwave7:397874137529319425')
-    .then(() => msg.react('a:parrotwave6:397874138959839233')) 
-    .then(() => msg.react('a:parrotwave5:397874134929113088')) 
-    .then(() => msg.react('a:parrotwave4:397874133523890178'))
-    .then(() => msg.react('a:parrotwave3:397874131539853322'))
-    .then(() => msg.react('a:parrotwave2:397874132664188930'))
-    .then(() => msg.react('a:parrotwave1:397874130185093131'));
-}
-
-// todo: make function for these block-y lettters
-function rizoPls(msg) {
-  msg.react('🇷')
-    .then(() => msg.react('🇮'))
-    .then(() => msg.react('🇿'))
-    .then(() => msg.react('🇴'))
-    .then(() => msg.react('🇵'))
-    .then(() => msg.react('🇱'))
-    .then(() => msg.react('🇸'));
-}
-
-function letsGoLetters(msg) {
-  msg.react('🇱')
-    .then(() => msg.react('🇪')) 
-    .then(() => msg.react('🇹')) 
-    .then(() => msg.react('🇸')) 
-    .then(() => msg.react('🇬')) 
-    .then(() => msg.react('🇴'));
-}
-
-String.prototype.strip = function() {
-  return this.toLowerCase().replace(/[\s']/g, "");
 }
 
 module.exports = runBot;
---
+++ responses.js
@@ -0,0 +1,112 @@
+const emojis = require('./emojis');
+const users = require('./users');
+
+function aaronGif(msg) {
+  // let's keep this bad boy around for a bit 
+  // msg.channel.send('https://giphy.com/gifs/man-pepsi-9kDr8Zm8dOuyc');
+  msg.channel.send('https://giphy.com/gifs/soda-funny-hot-G1zGMZtfmKjEQ');
+  msg.channel.send(`<@${users.aaron}> irl`);
+}
+
+function reactWave(msg) {
+  msg.react(emojis.wave7)
+    .then(() => msg.react(emojis.wave6))
+    .then(() => msg.react(emojis.wave5))
+    .then(() => msg.react(emojis.wave4))
+    .then(() => msg.react(emojis.wave3))
+    .then(() => msg.react(emojis.wave2))
+    .then(() => msg.react(emojis.wave1));
+}
+
+function wordReaction(msg, word) {
+  let initialPromise = msg.react(emojiLetter(word[0]));
+
+  for(let i = 1; i < word.length; i++)
+    initialPromise = initialPromise.then(() => msg.react(emojiLetter(word[i])));
+}
+
+function emojiLetter(letter) {
+  return emojis.letters[letter.toLowerCase()];
+}
+
+String.prototype.strip = function() {
+  return this.toLowerCase().replace(/[\s']/g, "");
+}
+
+module.exports = [
+  {
+    name: 'react to zack with zackMoji',
+    trigger: (msg, rnd) => msg.author.id === users.zack && rnd <= 0.2,
+    effect: (msg) => msg.react(emojis.zack)
+  },
+  {
+    name: '@zack',
+    trigger: (msg, rnd) => msg.content.includes(`@<${emojis.zack}>`),
+    effect: (msg) => msg.channel.send(`<@${users.zack}>`)
+  },
+  {
+    name: '@bacon',
+    trigger: (msg, rnd) => msg.content.includes(`@🥓`),
+    effect: (msg) => msg.channel.send(`<@${users.bacon}>`)
+  },
+  {
+    name: 'rizo parrot wave',
+    trigger: (msg, rnd) => msg.author.id === users.rizo && rnd < 0.05,
+    effect: reactWave
+  },
+  {
+    name: 'rizo party',
+    trigger: (msg, rnd) => msg.author.id === users.rizo && rnd >= 0.05 && rnd < 0.15,
+    effect: (msg) => msg.react(emojis.partyParrot)
+  },
+  {
+    name: 'rizo partyfast',
+    trigger: (msg, rnd) => msg.author.id === users.rizo && rnd >= 0.15 && rnd <= 0.2,
+    effect: (msg) => msg.react(emojis.fastParrot)
+  },
+  {
+    name: 'rizo pls',
+    trigger: (msg, rnd) => msg.content.includes(`<@${users.rizo}>`),
+    effect: (msg) => wordReaction(msg, "rizopls")
+  },
+  {
+    name: 'lets go party',
+    trigger: (msg, rnd) => (msg.content.strip().includes('letsgo') || msg.content.strip().includes('ketsgi')) && rnd < 0.5,
+    effect: (msg) => msg.react(emojis.partyParrot)
+  },
+  {
+    name: 'lets go letters',
+    trigger: (msg, rnd) => (msg.content.strip().includes('letsgo') || msg.content.strip().includes('ketsgi')) && rnd >= 0.5,
+    effect: (msg) => wordReaction(msg, "letsgo")
+  },
+  {
+    name: 'namelessGing',
+    trigger: (msg, rnd) => msg.author.id === users.jerran && rnd <= 0.2,
+    effect: (msg) => msg.react(emojis.wendyParrot)
+  },
+  {
+    name: 'aaron gif reply',
+    trigger: (msg, rnd) => msg.author.id === users.aaron && rnd <= 0.01,
+    effect: aaronGif 
+  },
+  {
+    name: 'aaron bepsi reply',
+    trigger: (msg, rnd) => msg.author.id === users.aaron && rnd <= 0.15,
+    effect: (msg) => msg.react(emojis.bepsi)
+  },
+  {
+    name: 'make [thing] great again',
+    trigger: (msg, rnd) => msg.content.toLowerCase().match(/.*ma[kd]e.*great again.*/),
+    effect: (msg) => msg.react(emojis.maga)
+  },
+  {
+    name: 'maga -> trumpgasm',
+    trigger: (msg, rnd) => msg.content.includes(':maga:'),
+    effect: (msg) => msg.react(emojis.trumpgasm)
+  },
+  {
+    name: 'siiick',
+    trigger: (msg, rnd) => msg.content === "🤢",
+    effect: (msg) => wordReaction(msg, "sick")
+  }
+];
