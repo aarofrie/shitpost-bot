974ca3df750589a580606319e8ed36b70e4f0d8b
Chris Bacon <cbacon91@gmail.com>
2018-03-14 11:33:36 -0600

    refactor to make triggers sensible to run

--- index.js
+++ index.js
@@ -31,13 +31,7 @@ function init() {
   const bot = new Discord.Client();
   const token = config.discord.authToken;
 
-  const baconId = '108352053692125184';
-  const zackId =  '108568431053246464';
-  const zackMoji = ':zack:401543766084943892';
-  const rizoId = '100758264047747072'
-  const parrotMoji = 'a:ultrafastparrot:397874139848769557';
-  const jerranId = '189006310501646336';
-  const aaronId = '65055432095301632';
+
 
   bot.login(token);
 
@@ -60,68 +54,129 @@ function init() {
 
     const rnd = Math.random();
 
-    if (msg.author.id === zackId && rnd <= 0.2) {
-      msg.react(zackMoji);
-    }
-    if (msg.content.includes(`@<:zack:401543766084943892>`)) {
-      msg.channel.send(`<@${zackId}>`);
-    }
-    if (msg.content.includes(`@🥓`)) {
-      msg.channel.send(`<@${baconId}>`);
-    }
-    if(msg.author.id === rizoId) {
-      if(rnd < 0.05)
-        msg.react('a:parrotwave7:397874137529319425')
-          .then(() => msg.react('a:parrotwave6:397874138959839233')) 
-          .then(() => msg.react('a:parrotwave5:397874134929113088')) 
-          .then(() => msg.react('a:parrotwave4:397874133523890178'))
-          .then(() => msg.react('a:parrotwave3:397874131539853322'))
-          .then(() => msg.react('a:parrotwave2:397874132664188930'))
-          .then(() => msg.react('a:parrotwave1:397874130185093131'));
-      else if (rnd >= 0.05 && rnd <= 0.15)
-        msg.react('a:partyparrot:397874122232954901');
-      else if (rnd >= 0.15 && rnd <= 0.2)
-        msg.react('a:ultrafastparrot:397874139848769557');
-    }
-    if(msg.content.includes('<@100758264047747072>') ) {
-        msg.react('🇷')
-          .then(() => msg.react('🇮'))
-          .then(() => msg.react('🇿'))
-          .then(() => msg.react('🇴'))
-          .then(() => msg.react('🇵'))
-          .then(() => msg.react('🇱'))
-          .then(() => msg.react('🇸'));
-    }
-    if(msg.content.strip().includes('letsgo') || msg.content.strip().includes('ketsgi')) {
-      if(rnd < 0.5)
-        msg.react('a:partyparrot:397874122232954901');
-      else {
-        msg.react('🇱')
-        .then(() => msg.react('🇪')) 
-        .then(() => msg.react('🇹')) 
-        .then(() => msg.react('🇸')) 
-        .then(() => msg.react('🇬')) 
-        .then(() => msg.react('🇴'));
-      }
-    }
-    if(msg.author.id === jerranId && rnd <= 0.2) {
-      msg.react('a:wendyparrot:399242434300870658');
-    }
-    if(msg.author.id === aaronId) {
-      if(rnd <= 0.001) {
-        // let's keep this bad boy around for a bit 
-        // msg.channel.send('https://giphy.com/gifs/man-pepsi-9kDr8Zm8dOuyc');
-        msg.channel.send('https://giphy.com/gifs/soda-funny-hot-G1zGMZtfmKjEQ');
-        msg.channel.send(`<@${aaronId}> irl`);
-      } else if (rnd <= 0.2)
-        msg.react(':bepsi:410166385918869504');
-    }
-    if(msg.content.toLowerCase().match(/.*ma[kd]e.*great again.*/)) {
-      msg.react(':maga:423325774674788352');
-    }
-    if(msg.content.includes(':maga:'))
-    msg.react(':trumpgasm:416058928502276106');
+    responses
+      .filter(r => r.trigger(msg, rnd))
+      .forEach(r => r.effect(msg));
   }
+}
+
+const baconId = '108352053692125184';
+const zackId =  '108568431053246464';
+const zackMoji = ':zack:401543766084943892';
+const rizoId = '100758264047747072'
+const parrotMoji = 'a:ultrafastparrot:397874139848769557';
+const jerranId = '189006310501646336';
+const aaronId = '65055432095301632';
+
+const responses = [
+  {
+    name: 'react to zack with zackMoji',
+    trigger: (msg, rnd) => msg.author.id === zackId && rnd <= 0.2,
+    effect: (msg) => msg.react(zackMoji)
+  },
+  {
+    name: '@zack',
+    trigger: (msg, rnd) => msg.content.includes(`@<:zack:401543766084943892>`),
+    effect: (msg) => msg.channel.send(`<@${zackId}>`)
+  },
+  {
+    name: '@bacon',
+    trigger: (msg, rnd) => msg.content.includes(`@🥓`),
+    effect: (msg) => msg.channel.send(`<@${baconId}>`)
+  },
+  {
+    name: 'rizo parrot wave',
+    trigger: (msg, rnd) => msg.author.id === rizoId && rnd < 0.05,
+    effect: reactWave
+  },
+  {
+    name: 'rizo party',
+    trigger: (msg, rnd) => msg.author.id === rizoId && rnd >= 0.05 && rnd < 0.15,
+    effect: (msg) => msg.react('a:partyparrot:397874122232954901')
+  },
+  {
+    name: 'rizo partyfast',
+    trigger: (msg, rnd) => msg.author.id === rizoId && rnd >= 0.15 && rnd <= 0.2,
+    effect: (msg) => msg.react('a:ultrafastparrot:397874139848769557')
+  },
+  {
+    name: 'rizo pls',
+    trigger: (msg, rnd) => msg.content.includes('<@100758264047747072>'),
+    effect: rizoPls
+  },
+  {
+    name: 'lets go party',
+    trigger: (msg, rnd) => (msg.content.strip().includes('letsgo') || msg.content.strip().includes('ketsgi')) && rnd < 0.5,
+    effect: (msg) => msg.react('a:partyparrot:397874122232954901')
+  },
+  {
+    name: 'lets go letters',
+    trigger: (msg, rnd) => (msg.content.strip().includes('letsgo') || msg.content.strip().includes('ketsgi')) && rnd >= 0.5,
+    effect: letsGoLetters
+  },
+  {
+    name: 'namelessGing',
+    trigger: (msg, rnd) => msg.author.id === jerranId && rnd <= 0.2,
+    effect: (msg) => msg.react('a:wendyparrot:399242434300870658')
+  },
+  {
+    name: 'aaron gif reply',
+    trigger: (msg, rnd) => msg.author.id === aaronId && rnd <= 0.01,
+    effect: aaronGif 
+  },
+  {
+    name: 'aaron bepsi reply',
+    trigger: (msg, rnd) => msg.author.id === aaronId && rnd <= 0.15,
+    effect: (msg) => msg.react(':bepsi:410166385918869504')
+  },
+  {
+    name: 'make [thing] great again',
+    trigger: (msg, rnd) => msg.content.toLowerCase().match(/.*ma[kd]e.*great again.*/),
+    effect: (msg) => msg.react(':maga:423325774674788352')
+  },
+  {
+    name: 'maga -> trumpgasm',
+    trigger: (msg, rnd) => msg.content.includes(':maga:'),
+    effect: (msg) => msg.react(':trumpgasm:416058928502276106')
+  }
+];
+
+function aaronGif(msg) {
+  // let's keep this bad boy around for a bit 
+  // msg.channel.send('https://giphy.com/gifs/man-pepsi-9kDr8Zm8dOuyc');
+  msg.channel.send('https://giphy.com/gifs/soda-funny-hot-G1zGMZtfmKjEQ');
+  msg.channel.send(`<@${aaronId}> irl`);
+}
+
+
+function reactWave(msg) {
+  msg.react('a:parrotwave7:397874137529319425')
+    .then(() => msg.react('a:parrotwave6:397874138959839233')) 
+    .then(() => msg.react('a:parrotwave5:397874134929113088')) 
+    .then(() => msg.react('a:parrotwave4:397874133523890178'))
+    .then(() => msg.react('a:parrotwave3:397874131539853322'))
+    .then(() => msg.react('a:parrotwave2:397874132664188930'))
+    .then(() => msg.react('a:parrotwave1:397874130185093131'));
+}
+
+// todo: make function for these block-y lettters
+function rizoPls(msg) {
+  msg.react('🇷')
+    .then(() => msg.react('🇮'))
+    .then(() => msg.react('🇿'))
+    .then(() => msg.react('🇴'))
+    .then(() => msg.react('🇵'))
+    .then(() => msg.react('🇱'))
+    .then(() => msg.react('🇸'));
+}
+
+function letsGoLetters(msg) {
+  msg.react('🇱')
+    .then(() => msg.react('🇪')) 
+    .then(() => msg.react('🇹')) 
+    .then(() => msg.react('🇸')) 
+    .then(() => msg.react('🇬')) 
+    .then(() => msg.react('🇴'));
 }
 
 String.prototype.strip = function() {
